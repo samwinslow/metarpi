b5ff693b2e0a44d6d575a41c62c1d0ea450f2f90
Sam Winslow <sammywinslow@gmail.com>
2022-04-06 16:21:13 -0400

    update

--- src/main.py
+++ src/main.py
@@ -4,21 +4,24 @@ import sh1106
 from fetch import fetch_metars
 from utils import chunk
 
+STATION = 'KSMQ'
 
 i2c = busio.I2C(board.SCL, board.SDA)
 display = sh1106.SH1106_I2C(128, 64, i2c, None, 0x3C)
 
-STATION = 'KSMQ'
-metar_string = fetch_metars(STATION)
-metar_lines = chunk(metar_string, 16)
-
 display.init_display()
 display.poweron()
 
+display.text("...", 0, 0)
+
+metar_string = fetch_metars(STATION)
+metar_lines = chunk(metar_string, 16)
+
+display.fill(0)
 display.text("Now: ", 0, 0)
 
 for i, line in enumerate(metar_lines):
-  y = (i * 8) + 1
+  y = (i + 1) * 8
   display.text(line, 0, y)
 
 display.show()
