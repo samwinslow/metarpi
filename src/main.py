e7e3e5d714a19069f5ab552f36cdda69e6732af3
Sam Winslow <sammywinslow@gmail.com>
2022-04-06 12:04:59 -0400

    0x78

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import board
 import busio
 from Adafruit_SSD1306 import SSD1306_128_64
 
-disp = SSD1306_128_64(None, i2c_address=0x3D)
+disp = SSD1306_128_64(None, i2c_address=0x78)
 
 disp.begin()
 disp.clear()
