dbe35d016577d85ae81592f50fee2fca1aee0a99
Sam Winslow <sammywinslow@gmail.com>
2022-04-06 15:54:50 -0400

    print to screen

--- src/main.py
+++ src/main.py
@@ -1,13 +1,21 @@
 import board
 import busio
 import sh1106
+from fetch import fetch_metars
+
 
 i2c = busio.I2C(board.SCL, board.SDA)
-
 display = sh1106.SH1106_I2C(128, 64, i2c, None, 0x3C)
+
+STATION = 'KSMQ'
+metar_string = fetch_metars(STATION)
+n = 16
+metar_lines = [metar_string[i:i+n] for i in range(0, len(metar_string), n)]
 
 display.init_display()
 display.poweron()
-# display.line(0,0,40,40,1)
-display.text("METAR Viewer Demo", 0, 0)
+
+for i, line in enumerate(metar_lines):
+  display.text(line, 0, i * 8)
+
 display.show()
