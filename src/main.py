8469f1191aa350103f6c7979b8d6cda43455e341
Sam Winslow <sammywinslow@gmail.com>
2022-04-06 11:57:53 -0400

    try without custom i2c

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ import board
 import busio
 from Adafruit_SSD1306 import SSD1306_128_64
 
-i2c = busio.I2C(board.SCL, board.SDA)
-oled = SSD1306_128_64(None, i2c=i2c)
+# i2c = busio.I2C(board.SCL, board.SDA)
+oled = SSD1306_128_64(None)
 
 oled.fill(1)
 oled.show()
