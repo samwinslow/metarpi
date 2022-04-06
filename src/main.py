37acead7291ea75f46ab4cdd145114d2276b76a4
Sam Winslow <sammywinslow@gmail.com>
2022-04-06 17:16:50 -0400

    cleanup

--- src/main.py
+++ src/main.py
@@ -7,12 +7,21 @@ from utils import chunk, time_now
 from gpiozero import Button
 
 STATION = 'KEWR'
+SLEEP_TIME_SECS = 30
 
 wake_button = Button(4)
 i2c = busio.I2C(board.SCL, board.SDA)
-display = sh1106.SH1106_I2C(128, 64, i2c, None, 0x3C)
+display = sh1106.SH1106_I2C(128, 64, i2c, None, 0x3C, 180)
 
 display.init_display()
+
+# Self-test sequence
+display.poweron()
+display.fill(1)
+display.show()
+sleep(2)
+display.fill(0)
+display.poweroff()
 
 while True:
   wake_button.wait_for_press()
@@ -32,6 +41,6 @@ while True:
     display.text(line, 0, y)
 
   display.show()
-  sleep(10)
+  sleep(SLEEP_TIME_SECS)
   display.fill(0)
   display.poweroff()
