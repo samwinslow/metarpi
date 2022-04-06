eddb4235546d6885667a3f0e4b3733b5aca304fb
Sam Winslow <sammywinslow@gmail.com>
2022-04-06 16:30:11 -0400

    current time

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ import board
 import busio
 import sh1106
 from fetch import fetch_metars
-from utils import chunk
+from utils import chunk, time_now
 
-STATION = 'KSMQ'
+STATION = 'KEWR'
 
 i2c = busio.I2C(board.SCL, board.SDA)
 display = sh1106.SH1106_I2C(128, 64, i2c, None, 0x3C)
@@ -18,7 +18,7 @@ metar_string = fetch_metars(STATION)
 metar_lines = chunk(metar_string, 16)
 
 display.fill(0)
-display.text("Now: ", 0, 0)
+display.text(f"Now: {time_now()}", 0, 0)
 
 for i, line in enumerate(metar_lines):
   y = (i + 1) * 8
